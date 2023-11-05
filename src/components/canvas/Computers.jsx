578597a0f3ac1ddae719a2d577c2f50946c72221
Shira Cohen <shira140@gmail.com>
2023-11-05 16:32:35 +0200

    animated scroll button

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,40 +7,25 @@ const RobotModel = () => {
   const robot = useGLTF('./robot/scene.gltf')
   // const robot = useGLTF("./desktop_pc/scene.gltf")
 
-  const [hovered, setHovered] = useState(false)
-
-
-  useEffect(() => {
-    document.body.style.cursor = hovered ? 'pointer' : 'auto'
-  }, [hovered])
-
-
-
   return (
-    <>
-
-      <mesh >
-        <hemisphereLight intensity={0.5} groundColor='black' />
-        <pointLight intensity={2} />
-        <spotLight
-          position={[-20, 50, 10]}
-          // position={[0, 0, 0]}
-          angle={0.12}
-          penumbra={1}
-          intensity={1}
-          castShadow
-          shadow-mapSize={1024}
-        />
-        <primitive
-          object={robot.scene}
-          scale={2.5}
-          // position={[-2, -2.8, 0]} 
-          position={[3, -2.5, 2]}
-          rotation={[-0.01, -0.2, 0.2]}
-        />
-
-      </mesh>
-    </>
+    <mesh >
+      <hemisphereLight intensity={0.5} groundColor='black' />
+      <pointLight intensity={2} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={robot.scene}
+        scale={2.5}
+        position={[3, -2.5, 2]}
+        rotation={[-0.01, -0.2, 0.2]}
+      />
+    </mesh>
   )
 }
 
@@ -52,7 +37,7 @@ const RobotCanvas = () => {
       camera={{ position: [-20, 10, 10], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader/>}>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
