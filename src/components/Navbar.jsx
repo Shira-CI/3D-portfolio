3af8e1861bfe695c1d7751e31c8e63319ff92a2b
Shira Cohen <shira140@gmail.com>
2023-11-21 17:01:48 +0200

    mobile menu bcg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
             onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
           />
           <div className={`${toggleMobileMenu ? 'flex' : 'hidden'}
-             p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+             p-6 blue-gradient  absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
             <ul className='list-none flex justify-end items-start flex-col gap-4'>
               {navLinks.map((link) => (
                 <li key={link.id}
